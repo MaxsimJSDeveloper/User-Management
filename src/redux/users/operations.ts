a20848e99b77fbae0eacab0a06a59d496e9f2731
Holovko Maksym <golovkomaksim852@gmail.com>
2024-08-30 19:55:59 +0300

    add-reduser-qwery

--- src/redux/users/operations.ts
+++ src/redux/users/operations.ts
@@ -1,4 +1,18 @@
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { User } from "../../types/general";
 
-// export const getUsers = createAsyncThunk();
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+
+export const getAllUsers = createAsyncThunk<
+  User[],
+  void,
+  { rejectValue: string }
+>("/users", async (_, thunkAPI) => {
+  try {
+    const res = await axios.get<User[]>("/users");
+    return res.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue((error as Error).message);
+  }
+});
--- src/redux/users/slice.ts
+++ src/redux/users/slice.ts
@@ -1,15 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, SerializedError } from "@reduxjs/toolkit";
+import { getAllUsers } from "./operations";
+import { User } from "../../types/general";
+
+interface UsersState {
+  users: User[];
+  isLoading: boolean;
+  error: string | null;
+}
+
+const initialState: UsersState = {
+  users: [],
+  isLoading: false,
+  error: null,
+};
+
+export const handlePending = (state: UsersState) => {
+  state.isLoading = true;
+};
+
+export const handleRejected = (
+  state: UsersState,
+  action: PayloadAction<
+    unknown,
+    string,
+    { arg: void; requestId: string; requestStatus: "rejected" },
+    SerializedError
+  >
+) => {
+  state.isLoading = false;
+  state.error = action.error.message || "Something went wrong";
+};
 
 const usersSlice = createSlice({
   name: "users",
-  initialState: {
-    items: [],
-    isLoading: false,
-    error: null,
-  },
+  initialState,
   reducers: {},
-  // extraReducers: (builder) => {
-  // },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllUsers.pending, handlePending)
+      .addCase(getAllUsers.rejected, handleRejected)
+      .addCase(
+        getAllUsers.fulfilled,
+        (state, action: PayloadAction<User[]>) => {
+          state.isLoading = false;
+          state.error = null;
+          state.users = action.payload;
+        }
+      );
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
