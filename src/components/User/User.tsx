665284ab54fdbc393f5181c88a4d67ea954733c0
Holovko Maksym <golovkomaksim852@gmail.com>
2024-08-31 16:55:21 +0300

    add-styles

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { UserProps } from "./User.types";
 import css from "./User.module.css";
+import { FiUser } from "react-icons/fi";
+import { MdEmail } from "react-icons/md";
+import { AiOutlinePhone } from "react-icons/ai";
+import { PiUserCircleLight } from "react-icons/pi";
 
 const User: React.FC<UserProps> = ({ user }) => {
   const { name, username, email, phone } = user;
 
   return (
     <div className={css.user}>
+      <div className={css.avatar}>
+        <PiUserCircleLight size={60} />
+      </div>
+
       <h3 className={css.title}>{name}</h3>
       <div className={css.userInfo}>
-        <p>Username: {username}</p>
-        <p>Email: {email}</p>
-        <p>Phone: {phone}</p>
+        <p className={css.infoItem}>
+          <FiUser /> {username}
+        </p>
+        <p className={css.infoItem}>
+          <MdEmail /> {email}
+        </p>
+        <p className={css.infoItem}>
+          <AiOutlinePhone /> {phone}
+        </p>
       </div>
     </div>
   );
